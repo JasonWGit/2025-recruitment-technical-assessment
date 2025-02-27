97ef23dd75d62ec14d158feb7a95eeb0fbe1539a
JasonW <jason.wang911666@gmail.com>
2025-02-27 23:21:10 +1100

    partially completed tasks

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: (recipe | ingredient)[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -46,23 +46,110 @@ app.post("/parse", (req:Request, res:Response) => {
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
   // TODO: implement me
-  return recipeName
+  let retString = recipeName.replace(/[-_]/g, ' ');
+  retString = retString.replace(/[^a-zA-Z\s]/g, '');
+  retString = retString.toLowerCase();
+  if (retString.length === 0) {
+    return null;
+  }
+  const wordsArray = retString.split(/\s+/);
+  for (let i = 0; i < wordsArray.length; i++) {
+    wordsArray[i] = wordsArray[i][0].toUpperCase() + wordsArray[i].slice(1);
+  }
+  retString = wordsArray.join(' ');
+
+  return wordsArray.join(' ');
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
   // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const entry = req.body;
+  if (entry.type !== 'recipe' && entry.type !== 'ingredient') {
+    return res.status(400).json({ error: 'Invalid type' });
+  } 
 
+  if (cookbook.map(item => item.name).includes(entry.name)) {
+    return res.status(400).json({ error: 'entry with that name already exists' });
+  }
+
+  if (entry.type == "recipe") {
+    return addRecipe(entry, res);
+  } else if (entry.type == "ingredient") {
+    return addIngredient(entry, res);
+  }
 });
+
+// Task 2 Helpers
+const addRecipe = (entry: recipe, res: Response) => {
+  const requiredItems = new Set();
+  for (const item of entry.requiredItems) {
+    if (requiredItems.has(item.name)) {
+      return res.status(400).json({ error: 'more than one element per name in requiredItems' });
+    } else {
+      requiredItems.add(item.name);
+    }
+  }
+  const newRecipe: recipe = { 
+    type: entry.type, 
+    name: entry.name,
+    requiredItems: entry.requiredItems,
+  };
+  cookbook.push(newRecipe);
+  return res.status(200).json({});
+}
+
+const addIngredient = (entry: ingredient, res: Response) => {
+  if (entry.cookTime < 0) {
+    return res.status(400).json({ error: 'Invalid cookTime' });
+  }
+
+  const newIngredient: ingredient = {
+    type: entry.type,
+    name: entry.name,
+    cookTime: entry.cookTime,
+  };
+  cookbook.push(newIngredient);
+  return res.status(200).json({});
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
   // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  // res.status(500).send("not yet implemented!")
+  const targetRecipeName = req.query.name as string;
+  const recipeIndex = cookbook.findIndex(recipe => recipe.name === targetRecipeName);
+  if (recipeIndex === -1) {
+    return res.status(400).json({ error: 'recipe with given name not found' });
+  }
+  if (cookbook[recipeIndex].type === 'ingredient') {
+    return res.status(400).json({ error: 'searched name is not name of recipe' });
+  }
 
+  const recipeSummary = [];
+  // for bfs, treating recipes as parent nodes and their requiredItems as child nodes
+  const queue = [];
+  queue.push(cookbook[recipeIndex].name);
+  
+  while (queue.length !== 0) {
+    let tempName = queue.shift();
+    const tempIndex = cookbook.findIndex(entry => entry.name === tempName);
+
+    if (tempIndex === -1) {
+      return res.status(400).json({ error: 'recipe contains recipe/ingredient not in cookbook' });
+    } 
+    recipeSummary.push(cookbook[tempIndex]);
+    if (cookbook[tempIndex].type === 'recipe') {
+      // force typescript to recognise cookbook[tempIndex] as recipe rather than either recipe or ingredient
+      let currRecipe = cookbook[tempIndex] as recipe;
+      for (const requiredItem of currRecipe.requiredItems) {
+        queue.push(requiredItem.name);
+      }
+    }
+  }
+  return res.status(200).json(recipeSummary);
 });
 
 // =============================================================================
